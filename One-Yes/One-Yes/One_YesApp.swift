ae33c92505ebc41eac2f685a5ef883e10340e7dd
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-07-11 12:42:17 -0600

    Initial Commit

---
+++ One-Yes/One-Yes/One_YesApp.swift
@@ -0,0 +1,17 @@
+//
+//  One_YesApp.swift
+//  One-Yes
+//
+//  Created by Brody Sears on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct One_YesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
